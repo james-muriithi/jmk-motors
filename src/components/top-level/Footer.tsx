d05b491b73400bf9cae88967911e151cfb263890
James Muriithi <26818458+james-muriithi@users.noreply.github.com>
2024-03-22 03:34:40 +0300

    disclaimer text

--- src/components/top-level/Footer.tsx
+++ src/components/top-level/Footer.tsx
@@ -1,21 +1,38 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 mt-20">
-      <div className="py-6 large-container">
-        <div className="flex justify-between items-center px-3">
-          <Link
-            to="/"
-            className="text-xl font-bold text-white hover:text-gray-200"
+    <>
+      <div className="max-w-[1600px] mx-auto mt-10">
+        <p className="py-2 text-white sm:py-0 text-sm">
+          <b className="uppercase">Disclaimer:</b> This website is a clone
+          developed solely for educational purposes. It is not intended for
+          commercial use or to compete with the
+          <a
+            href="https://kaiandkaro.com"
+            target="_blank"
+            className="text-violet-400 hover:underline"
           >
-            JMK Motors
-          </Link>
-          <p className="py-2 text-white sm:py-0 text-sm">
-            &copy; {new Date().getFullYear()} All rights reserved
-          </p>
-        </div>
+            &nbsp;original site (kaiandkaro.com)
+          </a>
+          . All content and trademarks referenced here belong to their
+          respective owners. This project is designed to showcase development
+          skills and to promote learning and understanding of web development
+          practices.
+        </p>
       </div>
-    </footer>
+      <footer className="bg-gray-800 mt-10">
+        <div className="py-6 max-w-[1600px] mx-auto">
+          <div className="flex justify-between items-center px-3">
+            <Link
+              to="/"
+              className="text-xl font-bold text-white hover:text-gray-200"
+            >
+              JMK Motors
+            </Link>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 }
