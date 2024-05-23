75640096c4f4369dc282b633ec2ac8da7728bbed
James Muriithi <26818458+james-muriithi@users.noreply.github.com>
2024-05-23 23:07:02 +0300

    refetch on slug change

--- src/pages/SingleVehicle.tsx
+++ src/pages/SingleVehicle.tsx
@@ -39,7 +39,7 @@ export default function SingleVehicle() {
     if(!vehicles.length) {
       dispatch(fetchVehicles({ firestore }));
     }
-  }, []);
+  }, [vehicleSlug]);
 
   const similarVehicles = vehicles
     .filter(({ slug }) => slug !== vehicleSlug)
